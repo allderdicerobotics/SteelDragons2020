785ab3b649fc27957c3cdc68cd9092415ca8d46e
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2020-02-07 11:53:18 -0500

    Added all Smart Motion code for IntakeFolding

--- SteelDragons2020/src/main/java/frc/robot/Subsystems/Intake.java
+++ SteelDragons2020/src/main/java/frc/robot/Subsystems/Intake.java
@@ -13,6 +13,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants;
@@ -22,8 +23,7 @@ public class Intake extends SubsystemBase {
   private final CANSparkMax intakeFoldMotor;
   private CANPIDController intakeFoldMotorPIDController;
   private CANEncoder intakeFoldMotorCANEncoder;
-  private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxAccel;
-  private double currentIntakeFoldPosition;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;  private double currentIntakeFoldPosition;
 
   private final CANSparkMax intakeSpinMotor;
 
@@ -34,15 +34,17 @@ public class Intake extends SubsystemBase {
 
     intakeSpinMotor = new CANSparkMax(Constants.CAN_SPIN_INTAKE, MotorType.kBrushless);
 
-    kP = 0.0;
+    kP = 0.00005;
     kI = 0.0;
     kD = 0.0;
     kIz = 0.0;
-    kFF = 0.0;
-    kMaxOutput = 0.0;
-    kMinOutput = 0.0;
+    kFF = 0.00005;
+    kMaxOutput = 1.0;
+    kMinOutput = -1.0;
+    maxRPM = 5700;
 
-    maxAccel = 0.0;
+    maxVel = 10000;
+    maxAcc = 25000;
 
     intakeFoldMotorPIDController.setP(kP);
     intakeFoldMotorPIDController.setI(kI);
@@ -51,19 +53,30 @@ public class Intake extends SubsystemBase {
     intakeFoldMotorPIDController.setFF(kFF);
     intakeFoldMotorPIDController.setOutputRange(kMinOutput, kMaxOutput);
 
-    intakeFoldMotorPIDController.setSmartMotionMaxAccel(maxAccel, 0);
+    int smartMotionSlot = 0;
+    intakeFoldMotorPIDController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
+    intakeFoldMotorPIDController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
+    intakeFoldMotorPIDController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
+    intakeFoldMotorPIDController.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
 
     // //Use this only for tuning PID values and testing
-    // SmartDashboard.putNumber("IntakeFold P", kP);
-    // SmartDashboard.putNumber("IntakeFold I", kI);
-    // SmartDashboard.putNumber("IntakeFold D" , kD);
+    // SmartDashboard.putNumber("IntakeFold P Gain", kP);
+    // SmartDashboard.putNumber("IntakeFold I Gain", kI);
+    // SmartDashboard.putNumber("IntakeFold D Gain" , kD);
     // SmartDashboard.putNumber("IntakeFold I Zone", kIz);
     // SmartDashboard.putNumber("IntakeFold Feed Forward", kFF);
     // SmartDashboard.putNumber("IntakeFold Max Output", kMaxOutput);
     // SmartDashboard.putNumber("IntakeFold Min Output", kMinOutput);
 
-    // SmartDashboard.putNumber("IntakeFold Max Accel", maxAccel);
+    // SmartDashboard.putNumber("IntakeFold Max Velocity", maxVel);
+    // SmartDashboard.putNumber("IntakeFold Min Velocity", minVel);
+    // SmartDashboard.putNumber("IntakeFold Max Acceleration", maxAcc);
+    // SmartDashboard.putNumber("IntakeFold Allowed Closed Loop Error", allowedErr);
     // SmartDashboard.putNumber("IntakeFold Set Position", 0);
+    // SmartDashboard.putNumber("IntakeFold Set Velocity", 0);
+
+    // // button to toggle between velocity and smart motion modes
+    // SmartDashboard.putBoolean("IntakeFold Mode", true);
   }
 
   //INTAKE SPIN
@@ -89,7 +102,7 @@ public class Intake extends SubsystemBase {
   }
 
   public void top() {
-    setPosition(300.0);
+    setPosition(-17.0);
   }
 
   public void setPosition(double position){
@@ -98,36 +111,54 @@ public class Intake extends SubsystemBase {
   }
 
   public void PIDSetup() {
-  //   //Use this only for tuning PID values and testing
-  //   double p = SmartDashboard.getNumber("IntakeFold P", 0);
-  //   double i = SmartDashboard.getNumber("IntakeFold I", 0);
-  //   double d = SmartDashboard.getNumber("IntakeFold D", 0);
-  //   double iz = SmartDashboard.getNumber("IntakeFold I Zone", 0);
-  //   double ff = SmartDashboard.getNumber("IntakeFold Feed Forward", 0);
-  //   double max = SmartDashboard.getNumber("IntakeFold Max Output", 0);
-  //   double min = SmartDashboard.getNumber("IntakeFold Min Output", 0);
+    // // read PID coefficients from SmartDashboard
+    // double p = SmartDashboard.getNumber("IntakeFold P Gain", 0);
+    // double i = SmartDashboard.getNumber("IntakeFold I Gain", 0);
+    // double d = SmartDashboard.getNumber("IntakeFold D Gain", 0);
+    // double iz = SmartDashboard.getNumber("IntakeFold I Zone", 0);
+    // double ff = SmartDashboard.getNumber("IntakeFold Feed Forward", 0);
+    // double max = SmartDashboard.getNumber("IntakeFold Max Output", 0);
+    // double min = SmartDashboard.getNumber("IntakeFold Min Output", 0);
+    // double maxV = SmartDashboard.getNumber("IntakeFold Max Velocity", 0);
+    // double minV = SmartDashboard.getNumber("IntakeFold Min Velocity", 0);
+    // double maxA = SmartDashboard.getNumber("IntakeFold Max Acceleration", 0);
+    // double allE = SmartDashboard.getNumber("IntakeFold Allowed Closed Loop Error", 0);
 
-  //   double maxA = SmartDashboard.getNumber("IntakeFold Max Accel", 0);
+    // // if PID coefficients on SmartDashboard have changed, write new values to controller
+    // if((p != kP)) { intakeFoldMotorPIDController.setP(p); kP = p; }
+    // if((i != kI)) { intakeFoldMotorPIDController.setI(i); kI = i; }
+    // if((d != kD)) { intakeFoldMotorPIDController.setD(d); kD = d; }
+    // if((iz != kIz)) { intakeFoldMotorPIDController.setIZone(iz); kIz = iz; }
+    // if((ff != kFF)) { intakeFoldMotorPIDController.setFF(ff); kFF = ff; }
+    // if((max != kMaxOutput) || (min != kMinOutput)) { 
+    //   intakeFoldMotorPIDController.setOutputRange(min, max); 
+    //   kMinOutput = min; kMaxOutput = max; 
+    // }
+    // if((maxV != maxVel)) { intakeFoldMotorPIDController.setSmartMotionMaxVelocity(maxV,0); maxVel = maxV; }
+    // if((minV != minVel)) { intakeFoldMotorPIDController.setSmartMotionMinOutputVelocity(minV,0); minVel = minV; }
+    // if((maxA != maxAcc)) { intakeFoldMotorPIDController.setSmartMotionMaxAccel(maxA,0); maxAcc = maxA; }
+    // if((allE != allowedErr)) { intakeFoldMotorPIDController.setSmartMotionAllowedClosedLoopError(allE,0); allowedErr = allE; }
 
-  //   if(p != kP) { kP = p; intakeFoldMotorPIDController.setP(p); }
-  //   if(i != kI) { kI = i; intakeFoldMotorPIDController.setI(i); }
-  //   if(d != kD) { kD = d; intakeFoldMotorPIDController.setD(d); }
-  //   if(iz != kIz) { kIz = iz; intakeFoldMotorPIDController.setIZone(iz);  }
-  //   if(ff != kFF) { kFF = ff; intakeFoldMotorPIDController.setFF(ff);}
-  //   if(max != kMaxOutput) || (min != kMinOutput) {
-  //    intakeFoldMotorPIDController.setOutputRange(min, max);
-  //    kMaxOutput = max; kMinOutput = min; }
+    // double setPoint, processVariable;
+    // boolean mode = SmartDashboard.getBoolean("IntakeFold Mode", false);
+    // if(mode) {
+    //   setPoint = SmartDashboard.getNumber("IntakeFold Set Velocity", 0);
+    //   intakeFoldMotorPIDController.setReference(setPoint, ControlType.kVelocity);
+    //   processVariable = intakeFoldMotorCANEncoder.getVelocity();
+    // } else {
+    //   setPoint = SmartDashboard.getNumber("IntakeFold Set Position", 0);
+    //   /**
+    //    * As with other PID modes, Smart Motion is set by calling the
+    //    * setReference method on an existing pid object and setting
+    //    * the control type to kSmartMotion
+    //    */
+    //   intakeFoldMotorPIDController.setReference(setPoint, ControlType.kSmartMotion);
+    //   processVariable = intakeFoldMotorCANEncoder.getPosition();
+    // }
 
-  //   if(maxA != maxAccel) { maxAccel = maxA; intakeFoldMotorPIDController.setSmartMotionMaxAccel(maxA, 0);}
-
-  //   double setPoint;
-  //   setPoint = SmartDashboard.getNumber("IntakeFold Set Position", 0);
-  
-  //   intakeFoldMotorPIDController.setReference(setPoint, ControlType.kSmartMotion);
-  
-  //   SmartDashboard.putNumber("Current IntakeFold Position: ", intakeFoldMotorCANEncoder.getPosition());
-
-  //   currentIntakeFoldPosition = intakeFoldMotorCANEncoder.getPosition();
+    // SmartDashboard.putNumber("IntakeFold SetPoint", setPoint);
+    // SmartDashboard.putNumber("IntakeFold Process Variable", processVariable);
+    // SmartDashboard.putNumber("IntakeFold Output", intakeFoldMotor.getAppliedOutput());
   }
 
   @Override
