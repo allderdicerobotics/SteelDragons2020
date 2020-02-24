4ca158ac8e2861a2c9b27f432c73cc04cad2a1dc
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-02-23 19:46:21 -0500

    Create SixBallAuto.java

---
+++ SteelDragons2020/src/main/java/frc/robot/Commands/Autonomous/SixBallAuto.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Commands.Autonomous;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.Subsystems.DriveTrain;
+
+public class DriveOffLine extends CommandBase {
+  /**
+   * Creates a new DriveOffLine.
+   */
+  double startTime;
+  double driveTime = 2.0;
+  DriveTrain driveTrain;
+  public DriveOffLine() {
+    this.driveTrain = RobotContainer.driveTrain;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.driveTrain.stop();
+    startTime = Timer.getFPGATimestamp();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(Timer.getFPGATimestamp() > startTime + driveTime) {
+      this.driveTrain.arcadeDrive(-0.4, 0.0);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    this.driveTrain.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
