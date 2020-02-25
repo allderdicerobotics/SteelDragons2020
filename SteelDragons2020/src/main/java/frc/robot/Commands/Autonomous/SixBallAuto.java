c226a4a5574c462e4bd5d98d0a8abc9215ca78e8
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2020-02-25 15:37:02 -0500

    add sixballauto command

--- SteelDragons2020/src/main/java/frc/robot/Commands/Autonomous/SixBallAuto.java
+++ SteelDragons2020/src/main/java/frc/robot/Commands/Autonomous/SixBallAuto.java
@@ -1,3 +1,4 @@
+/*----------------------------------------------------------------------------*/
 /* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -6,48 +7,29 @@
 
 package frc.robot.Commands.Autonomous;
 
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
-import frc.robot.Subsystems.DriveTrain;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Commands.Autonomous.*;
+import frc.robot.Commands.Shooting.AutoShoot;
 
-public class DriveOffLine extends CommandBase {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class SixBallAuto extends SequentialCommandGroup {
   /**
-   * Creates a new DriveOffLine.
+   * Creates a new SixBallAuto.
    */
-  double startTime;
-  double driveTime = 2.0;
-  DriveTrain driveTrain;
-  public DriveOffLine() {
-    this.driveTrain = RobotContainer.driveTrain;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(this.driveTrain);
-  }
+  public SixBallAuto() {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    addCommands(
+      new DriveOffLine(),
+      new AutoShoot(true),
+      new FetchBall(),
+      new FetchBall(),
+      new FetchBall(),
+      new AutoShoot(true)
+    );
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    this.driveTrain.stop();
-    startTime = Timer.getFPGATimestamp();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if(Timer.getFPGATimestamp() > startTime + driveTime) {
-      this.driveTrain.arcadeDrive(-0.4, 0.0);
-    }
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    this.driveTrain.stop();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }
